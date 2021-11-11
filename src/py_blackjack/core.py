96d5871f858a842211587888d16392f8390b0bf6
Cian Laguesma <cbalaguesma1101@gmail.com>
2021-11-12 00:36:39 +0800

    added 2 strats

--- src/py_blackjack/core.py
+++ src/py_blackjack/core.py
@@ -1,5 +1,5 @@
 import hand
-
+import strat
 # import add_card, deal, new_hand, up_card import hand
 
 
@@ -7,12 +7,12 @@ def stupid_strategy(p_hand, opponent_up_card):
     return hand.total([opponent_up_card]) > 5
 
 
-def stop_at_17(hand, opponent_up_card):
-    pass
+# def stop_at_17(hand, opponent_up_card):
+#     pass
 
 
-def stop_at(n):
-    pass
+# def stop_at(n):
+#     pass
 
 
 def play_hand(strategy, p_hand, opponent_up_card):
@@ -42,10 +42,44 @@ def play_game(player_strategy, house_strategy):
         )
         if hand.total(house_hand) > 21:
             return 1
-        elif hand.total(play_hand) > hand.total(house_hand):
+        elif hand.total(player_hand) > hand.total(house_hand):
             return 1
         else:
             return 0
 
+def play_hand2(strategy, p_hand, opponent_up_card, h_hand):
+    if hand.total(p_hand) > 21:
+        return p_hand
+    elif strategy(p_hand, h_hand):
+        # recursion
+        if strategy(p_hand, h_hand):
+            hand.add_card(p_hand, hand.deal())
+            play_hand2(strategy, p_hand, opponent_up_card, h_hand)
+        return p_hand
+    else:
+        return p_hand
 
-print(play_game(stupid_strategy, stupid_strategy))
+
+def play_game2(player_strategy, house_strategy):
+    house_initial_hand = hand.new_hand()
+    player_hand = play_hand2(
+        player_strategy, hand.new_hand(), hand.up_card(house_initial_hand), house_initial_hand
+    )
+    # print (hand.total(house_hand))
+    if hand.total(player_hand) > 21:
+        return 0
+    else:
+        placeholder = 0
+        house_hand = play_hand2(
+            house_strategy, house_initial_hand, hand.up_card(player_hand), placeholder
+        )
+        if hand.total(house_hand) > 21:
+            return 1
+        elif hand.total(player_hand) > hand.total(house_hand):
+            return 1
+        else:
+            return 0
+print ("#### GAME 1 ####")
+print(play_game(stupid_strategy,strat.stop_at_17))
+print ("#### GAME 2 ####")
+print(play_game2(strat.dealer_hand, strat.stop_at_17))
--- src/py_blackjack/strat.py
+++ src/py_blackjack/strat.py
@@ -1,9 +1,42 @@
 #stop-at-17
+import hand
 
-def stop_at_17():
-    if total < 17:
-        hit()
+def stop_at_17(p_hand,opponent_up_card):
+    if hand.total(p_hand) < 17:
+        print (hand.total(p_hand))
+        return True
 
-def stop_at_n(int n):
-    if total < n:
-        hit()
+
+def dealer_hand(p_hand,h_hand):
+    checker = True
+    
+    if (hand.total(p_hand) == 12 and (hand.total(h_hand) <=6 or hand.total(h_hand) >=4)):
+        checker = False
+    if ((hand.total(p_hand) == 13 or hand.total(p_hand) == 14) and hand.total(h_hand) <=6):
+        checker = False
+    if (hand.total(p_hand) == 15 and hand.total(h_hand) <= 6):
+        checker = False
+    if (hand.total(p_hand)== 16 and hand.total(h_hand) <= 6):
+        checker = False
+    if (hand.total(p_hand)== 17 and hand.total(h_hand)<=10):
+        checker = False
+    if ((hand.total(p_hand) >= 18 and hand.total(p_hand) <= 21) and hand.total(h_hand) <= 10):
+        checker = False
+    if (hand.total(p_hand)<=11 and hand.total(p_hand)>=5):
+        checker = True
+    if checker == False:
+        return False
+    else:
+        return True
+# def stop_at_what(n):
+#     if hand.total([opponent_up_card]) < n:
+#         return True
+
+# def stop_at_n(n):
+#     if hand.total([opponent_up_card]) < n:
+#         return stop_at_what
+#     else:
+#         return True
+
+# def take_into(p_hand,h_hand):
+#     if count of dealer is < 6; your hand stop at 13 
