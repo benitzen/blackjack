63d9815be46206dd75c44dcb51ee1eb32870e252
Tomas Alejandro V. Falgui III <tomyfalguiiii@yahoo.com>
2021-11-11 16:11:07 +0800

    add option to specify which hand

--- src/py_blackjack/hand.py
+++ src/py_blackjack/hand.py
@@ -1,6 +1,9 @@
 import random
+
 official_deck = []
 hand = []
+
+
 def full_deck():
     suit = []
     for i in range(4):
@@ -15,22 +18,24 @@ def full_deck():
         print(suitName)
         for j in range(13):
             if j < 9:
-                suit.append([suitName,str(j+2)])
+                suit.append([suitName, str(j + 2)])
             elif j == 9:
-                suit.append([suitName,"J"])
+                suit.append([suitName, "J"])
             elif j == 10:
-                suit.append([suitName,"Q"])
+                suit.append([suitName, "Q"])
             elif j == 11:
-                suit.append([suitName,"K"])
+                suit.append([suitName, "K"])
             else:
-                suit.append([suitName,"A"])
+                suit.append([suitName, "A"])
     return suit
+
 
 def n_shuffled_decks(n):
     deck = full_deck()
     deck *= n
     random.shuffle(deck)
     return deck
+
 
 def reset_deck():
     resetted_deck = n_shuffled_decks(4)
@@ -45,27 +50,37 @@ def deal():
     random.shuffle(official_deck)
     return dealt_hand
 
+
 def new_hand():
     hand.append(deal())
 
-def up_card():
-    return hand.pop()
+
+def up_card(given_hand):
+    return given_hand.pop()
+
 
 def add_card(hand, card):
     hand.insert(0, card)
 
+
 def total(hand):
     total_num = 0
     for i in range(len(hand)):
-        if hand[i][1] == "A" or hand[i][1] == "J" or hand[i][1] == "Q" or hand[i][1] == "K":
+        if (
+            hand[i][1] == "A"
+            or hand[i][1] == "J"
+            or hand[i][1] == "Q"
+            or hand[i][1] == "K"
+        ):
             total_num += 10
         else:
             total_num += int(hand[i][1])
     return total_num
-            
+
 
 print(len(full_deck()))
 print("------------------------------------------------")
 print(len(n_shuffled_decks(2)))
 print("------------------------------------------------")
 print(n_shuffled_decks(2))
+
